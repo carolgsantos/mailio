c2cb69ac3c46da58b85b2d0e5809843d3f1a4526
Carolina Galvão <carolinagalvaosantos@gmail.com>
2018-06-13 18:45:33 -0300

    Fixing boost library links

--- cmake/req.cmake
+++ cmake/req.cmake
@@ -1,6 +1,8 @@
 
 if(WIN32)
-    SET(Boost_USE_STATIC_LIBS ON)
+    # Add dyn link only for shared library
+	add_definitions(-DBOOST_ALL_NO_LIB -DBOOST_ALL_DYN_LINK)
+    #SET(Boost_USE_STATIC_LIBS ON)
 endif(WIN32)
 find_package(Boost REQUIRED COMPONENTS system date_time regex random)
 
